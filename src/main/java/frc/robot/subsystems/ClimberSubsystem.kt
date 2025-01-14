fb1a070f3732bf5a408aafb52f281af309357979
Noah Dombrowski <dombrowskinoah@gmail.com>
2025-01-14 16:45:41 -0500

    feat(climber): initialize climber subsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.kt
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.kt
@@ -1,35 +1,25 @@
 package frc.robot.subsystems
 
-import com.ctre.phoenix6.hardware.TalonFX
-import com.ctre.phoenix6.signals.NeutralModeValue
-import edu.wpi.first.units.Units
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.Constants
 
 
 object ClimberSubsystem : SubsystemBase() {
     private var extended: Boolean = false
-    private val motor: TalonFX = TalonFX(2, "rio")
     // motor position constants are located in Constants
-
-    init {
-        motor.setNeutralMode(NeutralModeValue.Brake)
-
-    }
 
     // Set motor to lowered position
     fun disengage(): Command = runOnce {
         extended = false
 
-        motor.setPosition(Units.Degrees.of(Constants.ClimberConstants.LOWERED_POS))
+        // TODO: Move motor to pos
     }
 
-    // Set motor to raised position
+    // Set motor to raised positon
     fun engage(): Command = runOnce {
         extended = true
 
-        motor.setPosition(Units.Degrees.of(Constants.ClimberConstants.EXTENDED_POS))
+        // TODO: move motor to pos
     }
 
 
