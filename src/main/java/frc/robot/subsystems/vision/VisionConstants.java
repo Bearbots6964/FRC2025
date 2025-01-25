33a9d13802ffa5a47975896b2f47eff7c4143779
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-01-25 12:09:50 -0500

    feat(vision): update camera names and transforms for improved vision processing

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -17,19 +17,29 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.units.Units;
 
 public class VisionConstants {
+
     // AprilTag layout
     public static AprilTagFieldLayout aprilTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
     // Camera names, must match names configured on coprocessor
-    public static String camera0Name = "camera_0";
-    public static String camera1Name = "camera_1";
+    public static String camera0Name = "Front_Camera";
+    public static String camera1Name = "Left_Camera";
 
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 = new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
-    public static Transform3d robotToCamera1 = new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+    public static Transform3d robotToCamera0 = new Transform3d(
+            Units.Inches.of((29.5 / 2) - (1.0 + (double) 1 / 8)),
+            Units.Inches.of(-0.5),
+            Units.Inches.of(5.0 + (double) 1 / 2),
+            new Rotation3d(Units.Degrees.of(0), Units.Degrees.of(-10.0), Units.Degrees.of(0)));
+    public static Transform3d robotToCamera1 = new Transform3d(
+            Units.Inches.of(-0.5),
+            Units.Inches.of((29.5 / 2) - (1.0 + (double) 1 / 8)),
+            Units.Inches.of(5.0 + (double) 1 / 2),
+            new Rotation3d(Units.Degrees.of(10.0), Units.Degrees.of(0.0), Units.Degrees.of(90.0)));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;
