481eb8813c6218188c17e91b48b6059b15bcf87f
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-02-19 08:18:03 -0500

    feat(elevator): add SysId support, tune elevator constants

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -9,34 +9,35 @@ import edu.wpi.first.units.measure.Voltage;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ElevatorIO {
-  @AutoLog
-  public class ElevatorIOInputs {
-    public boolean leftMotorConnected = false;
-    public Temperature leftMotorTemperature = Units.Celsius.of(0.0);
-    public Angle leftMotorPosition = Units.Degrees.of(0.0);
-    public AngularVelocity leftMotorVelocity = Units.DegreesPerSecond.of(0.0);
-    public Voltage leftMotorVoltage = Units.Volts.of(0.0);
-    public Current leftMotorCurrent = Units.Amps.of(0.0);
+    @AutoLog
+    public class ElevatorIOInputs {
+        public boolean leftMotorConnected = false;
+        public Temperature leftMotorTemperature = Units.Celsius.of(0.0);
+        public Angle leftMotorPosition = Units.Degrees.of(0.0);
+        public AngularVelocity leftMotorVelocity = Units.DegreesPerSecond.of(0.0);
+        public Voltage leftMotorVoltage = Units.Volts.of(0.0);
+        public Current leftMotorCurrent = Units.Amps.of(0.0);
 
-    public boolean rightMotorConnected = false;
-    public Temperature rightMotorTemperature = Units.Celsius.of(0.0);
-    public Angle rightMotorPosition = Units.Degrees.of(0.0);
-    public AngularVelocity rightMotorVelocity = Units.DegreesPerSecond.of(0.0);
-    public Voltage rightMotorVoltage = Units.Volts.of(0.0);
-    public Current rightMotorCurrent = Units.Amps.of(0.0);
+        public boolean rightMotorConnected = false;
+        public Temperature rightMotorTemperature = Units.Celsius.of(0.0);
+        public Angle rightMotorPosition = Units.Degrees.of(0.0);
+        public AngularVelocity rightMotorVelocity = Units.DegreesPerSecond.of(0.0);
+        public Voltage rightMotorVoltage = Units.Volts.of(0.0);
+        public Current rightMotorCurrent = Units.Amps.of(0.0);
 
-    public AngularVelocity targetVelocity = Units.RotationsPerSecond.of(0.0);
-    public Angle targetPosition = Units.Degrees.of(0.0);
-  }
+        public AngularVelocity targetVelocity = Units.RotationsPerSecond.of(0.0);
+        public Angle targetPosition = Units.Degrees.of(0.0);
+    }
 
-  default void updateInputs(ElevatorIOInputs inputs) {}
+    default void updateInputs(ElevatorIOInputs inputs) {}
 
-  default void setOpenLoop(double output) {}
+    default void setOpenLoop(double output) {}
 
-  default void setVelocity(AngularVelocity velocity) {}
+    default void setVelocity(AngularVelocity velocity) {}
 
-  default void setPosition(Angle position) {}
+    default void setPosition(Angle position) {}
 
-  default void stop() {}
+    default void setVoltage(Voltage voltage) {}
 
+    default void stop() {}
 }
