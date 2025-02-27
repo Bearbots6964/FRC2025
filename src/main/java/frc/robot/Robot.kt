1b2d7d07f4f8faf8ce7db687ff2463d3daf04593
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-02-27 12:19:57 -0500

    feat(formatting, constants, tuning): tune drive values with value for new base, along with some other cool and fun things

--- src/main/java/frc/robot/Robot.kt
+++ src/main/java/frc/robot/Robot.kt
@@ -15,6 +15,7 @@ package frc.robot
 import com.ctre.phoenix6.swerve.SwerveModuleConstants
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.DriveMotorArrangement
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement
+import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.Threads
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
@@ -94,12 +95,13 @@ class Robot : LoggedRobot() {
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our autonomous chooser on the dashboard.
         robotContainer = RobotContainer()
+        DriverStation.silenceJoystickConnectionWarning(true)
     }
 
     /** This function is called periodically during all modes.  */
     override fun robotPeriodic() {
         // Switch thread to high priority to improve loop timing
-        Threads.setCurrentThreadPriority(true, 99)
+        //Threads.setCurrentThreadPriority(true, 99)
 
         // Runs the Scheduler. This is responsible for polling buttons, adding
         // newly-scheduled commands, running already-scheduled commands, removing
@@ -109,7 +111,7 @@ class Robot : LoggedRobot() {
         CommandScheduler.getInstance().run()
 
         // Return to normal thread priority
-        Threads.setCurrentThreadPriority(false, 10)
+        //Threads.setCurrentThreadPriority(false, 10)
     }
 
     /** This function is called once when the robot is disabled.  */
