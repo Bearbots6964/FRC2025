02767097364426d58abc3cc2e9d223ca896fb2c8
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-03-14 09:51:08 -0400

    refactor(ReefPositionCommands): simplify state references by using direct constants for elevator and arm states

--- src/main/java/frc/robot/commands/ReefPositionCommands.kt
+++ src/main/java/frc/robot/commands/ReefPositionCommands.kt
@@ -2,56 +2,57 @@ package frc.robot.commands
 
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.Constants
+import frc.robot.Constants.ArmConstants
+import frc.robot.Constants.ElevatorConstants.ElevatorState
 import frc.robot.RobotContainer
 import frc.robot.subsystems.arm.Arm
 import frc.robot.subsystems.elevator.Elevator
 
 object ReefPositionCommands {
-    var reefPosition = Constants.ElevatorConstants.ElevatorState.L1
+    var reefPosition = ElevatorState.L1
     fun l1(e: Elevator, a: Arm): Command {
         RobotContainer.statusTopic.set("L1")
-        reefPosition = Constants.ElevatorConstants.ElevatorState.L1
-        return e.goToPosition(Constants.ElevatorConstants.ElevatorState.L1)
-            .andThen(a.moveArmToAngle(Constants.ArmConstants.ArmState.L1))
+        reefPosition = ElevatorState.L1
+        return e.goToPosition(ElevatorState.L1)
+            .andThen(a.moveArmToAngle(ArmConstants.ArmState.L1))
     }
     fun l2(e: Elevator, a: Arm): Command {
         RobotContainer.statusTopic.set("L2")
-        reefPosition = Constants.ElevatorConstants.ElevatorState.L2
-        return e.goToPosition(Constants.ElevatorConstants.ElevatorState.L2)
-            .andThen(a.moveArmToAngle(Constants.ArmConstants.ArmState.L2))
+        reefPosition = ElevatorState.L2
+        return e.goToPosition(ElevatorState.L2)
+            .andThen(a.moveArmToAngle(ArmConstants.ArmState.L2))
     }
     fun l3(e: Elevator, a: Arm): Command {
         RobotContainer.statusTopic.set("L3")
-        reefPosition = Constants.ElevatorConstants.ElevatorState.L3
-        return e.goToPosition(Constants.ElevatorConstants.ElevatorState.L3)
-            .andThen(a.moveArmToAngle(Constants.ArmConstants.ArmState.L3))
+        reefPosition = ElevatorState.L3
+        return e.goToPosition(ElevatorState.L3)
+            .andThen(a.moveArmToAngle(ArmConstants.ArmState.L3))
     }
     fun l4(e: Elevator, a: Arm): Command {
         RobotContainer.statusTopic.set("L4")
-        reefPosition = Constants.ElevatorConstants.ElevatorState.L4
-        return e.goToPosition(Constants.ElevatorConstants.ElevatorState.L4)
-            .andThen(a.moveArmToAngle(Constants.ArmConstants.ArmState.L4))
+        reefPosition = ElevatorState.L4
+        return e.goToPosition(ElevatorState.L4)
+            .andThen(a.moveArmToAngle(ArmConstants.ArmState.L4))
     }
     fun coralStationPosition(e: Elevator, a: Arm): Command {
         RobotContainer.statusTopic.set("Coral Station Position")
         return e.goToPosition(Constants.ElevatorConstants.CORAL_PICKUP)
-            .andThen(a.moveArmToAngle(Constants.ArmConstants.ArmState.CORAL_PICKUP))
+            .andThen(a.moveArmToAngle(ArmConstants.ArmState.CORAL_PICKUP))
     }
     fun home(e: Elevator, a: Arm): Command {
         RobotContainer.statusTopic.set("Home")
-        return e.goToPosition(Constants.ElevatorConstants.ElevatorState.HOME)
-            .alongWith(a.moveArmToAngle(Constants.ArmConstants.ArmState.HOME))
+        return e.goToPosition(ElevatorState.HOME)
+            .alongWith(a.moveArmToAngle(ArmConstants.ArmState.HOME))
     }
 
-    fun goToPosition(e: Elevator, a: Arm, s: Constants.ElevatorConstants.ElevatorState): Command {
+    fun goToPosition(e: Elevator, a: Arm, s: ElevatorState): Command {
         return when (s) {
-            Constants.ElevatorConstants.ElevatorState.L1 -> l1(e, a)
-            Constants.ElevatorConstants.ElevatorState.L2 -> l2(e, a)
-            Constants.ElevatorConstants.ElevatorState.L3 -> l3(e, a)
-            Constants.ElevatorConstants.ElevatorState.L4 -> l4(e, a)
-            Constants.ElevatorConstants.ElevatorState.CORAL_PICKUP -> coralStationPosition(e, a)
-            Constants.ElevatorConstants.ElevatorState.HOME -> home(e, a)
-            else -> l1(e, a)
+            ElevatorState.L1 -> l1(e, a)
+            ElevatorState.L2 -> l2(e, a)
+            ElevatorState.L3 -> l3(e, a)
+            ElevatorState.L4 -> l4(e, a)
+            ElevatorState.CORAL_PICKUP -> coralStationPosition(e, a)
+            ElevatorState.HOME -> home(e, a)
         }
     }
 }
